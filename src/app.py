72f2c7c68ba34c31bae2acf767b382071b55f2b0
miguehm <miguel.hernandezmor@alumno.buap.mx>
2025-05-23 20:47:05 -0600

    fix return response

--- src/app.py
+++ src/app.py
@@ -18,25 +18,25 @@ def lambda_handler(event, context):
     cotizacion: str = body.get("cotizacion")
 
     if not number:
-        error_response(400, "Falta el 'phone-number' en la solicitud")
+        return error_response(400, "Falta el 'phone-number' en la solicitud")
 
     if not code:
-        error_response(400, "Falta el 'code' en la solicitud")
+        return error_response(400, "Falta el 'code' en la solicitud")
 
     if not cotizacion:
-        error_response(400, "Falta la 'cotizacion' en la solicitud")
+        return error_response(400, "Falta la 'cotizacion' en la solicitud")
 
     try:
         conn = get_db_connection()
         is_correct = verify_code(conn, number, code, cotizacion)
 
         if not is_correct:
-            error_response(
+            return error_response(
                 400,
                 "El codigo no es correcto o el número no esta en un proceso de verificación",
             )
 
-        success_response("El código es correcto")
+        return success_response("El código es correcto")
     except Exception as e:
         print(e)
         return error_response(500, "Server Eror")
