a6966aef2aaea0a4eb1896f5cb396de7419327b4
miguehm <miguel.hernandezmor@alumno.buap.mx>
2025-05-23 18:22:48 -0600

    update package script

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,50 +1,6 @@
 #!/bin/bash
-
 set -e
 
-if [ ! -d "build" ]; then
-    mkdir -p build
-    echo "build folder created"
-fi
-
-# echo "Installing requests library"
-# pip install requests -t ./build
-
-echo "Installing psycopg2 library"
-pip install psycopg2-binary -t ./build
-
-if [ ! -d "src" ]; then
-    echo "Error: src/ does not exist"
-    exit 1
-fi
-
-if [ ! "$(ls -A src/*.py 2>/dev/null)" ]; then
-    echo ".py files not found in src/"
-    exit 0
-fi
-
-cp src/*.py build/
-
-if [ $? -eq 0 ]; then
-    echo ".py copied successfully in build/"
-    echo "files copied:"
-    ls -la build/*.py
-else
-    echo "Error copying files"
-    exit 1
-fi
-
-zip_filename="build_package_$(date +%Y%m%d_%H%M%S).zip"
-cd build && zip -r "../$zip_filename" .
-cd ..
-
-if [ $? -eq 0 ]; then
-    echo "File $zip_filename created successfully"
-    echo "zip size:"
-    ls -lh "$zip_filename" | awk '{print $5}'
-else
-    echo "Error creating zip file"
-    exit 1
-fi
-
-rm -rf build
+echo "Empaquetando función Lambda..."
+zip -r lambda_function.zip src/ config/ -x "*/__pycache__/*" "*.pyc"
+echo "Empaquetado completo."
